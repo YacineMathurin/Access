74bd9e9bfcd47f81177d3a646eb92523b6d9708e
Abdouramane <abdouwer93@gmail.com>
2022-05-20 15:22:07 +0200

    Port set to suit heroku

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ require("./startup/prod")(app);
 // const p = Promise.reject(new Error("Miserably failed !"));
 // p.then(() => console.log("Done"));
 
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => winston.info(`Listening on port ${port}`));

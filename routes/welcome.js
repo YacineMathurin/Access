e27fb03c81a7d8978623a5b70591405f29a98252
Abdouramane <abdouwer93@gmail.com>
2022-05-20 15:38:04 +0200

    Backend remote name changed

--- routes/welcome.js
+++ routes/welcome.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require("../middlewares/auth");
 
 router.get("/", async (req, res) => {
-  res.status(200).send({message: "Welcome !"});
+  res.status(200).send({message: "Welcome to you !"});
 });
 
 module.exports = router;
